648201ff27b108b5c628f39780eb83a5bf5797eb
supergrecko <me@supergrecko.com>
2019-09-16 21:20:46 +0200

    patch(0.0.0): Add bot config pojo

---
+++ src/main/kotlin/com/supergrecko/questionbot/dataclasses/BotConfig.kt
@@ -0,0 +1,27 @@
+package com.supergrecko.questionbot.dataclasses
+
+/**
+ * Represent the bot configuration
+ *
+ * @property owner bot owner snowflake
+ * @property prefix the bot command prefix
+ * @property guilds the guilds the bot views
+ */
+data class BotConfig(
+        val owner: String = "<missing id>",
+        val prefix: String = "$",
+        val guilds: MutableList<GuildConfig> = mutableListOf()
+)
+
+/**
+ * Represent a guild
+ *
+ * @property guild the guild snowflake
+ * @property role the required role to use snowflake
+ * @property count amount of questions asked in this guild
+ */
+data class GuildConfig(
+        val guild: String = "<missing id>",
+        val role: String = "<missing id>",
+        val count: Int = 0
+)
