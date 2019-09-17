3429766421d1379e9e3b39aaa8c0835c8086bc02
supergrecko <me@supergrecko.com>
2019-09-17 17:15:15 +0200

    patch(0.0.0): Complete bot config object

--- src/main/kotlin/com/supergrecko/questionbot/dataclasses/BotConfig.kt
+++ src/main/kotlin/com/supergrecko/questionbot/dataclasses/BotConfig.kt
@@ -14,7 +14,11 @@ data class BotConfig(
         val owner: String = "<missing id>",
         val prefix: String = "$",
         val guilds: MutableList<GuildConfig> = mutableListOf()
-)
+) {
+    fun addGuild(guild: GuildConfig) = guilds.add(guild)
+    fun get(id: String) = guilds.firstOrNull { it.guild == id }
+    fun removeGuild(id: String) = guilds.removeAll { it.guild == id }
+}
 
 /**
  * Represent a guild
@@ -26,5 +30,22 @@ data class BotConfig(
 data class GuildConfig(
         val guild: String = "<missing role>",
         val role: String = "<missing id>",
-        val count: Int = 0
+        val count: Int = 0,
+        val questions: MutableList<Question> = mutableListOf()
+)
+
+/**
+ * Represent a question
+ *
+ * @property sender the uid who sent the question
+ * @property channel the channel this was sent to
+ * @property id the guild-level question id
+ * @property question the question which was asked.
+ */
+data class Question(
+        val sender: String = "<missing id>",
+        val channel: String = "<missing channel>",
+        val id: Int = 0,
+        val responses: MutableList<String> = mutableListOf(),
+        val question: String = "<missing question>"
 )
