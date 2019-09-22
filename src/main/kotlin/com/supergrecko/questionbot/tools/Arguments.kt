8a2f849f260865364b2edc0c5eb36f7ce52f7e2b
supergrecko <me@supergrecko.com>
2019-09-22 11:33:30 +0200

    refactor(0.0.0): Make Arguments.asType null safe by providing an onError handler

--- src/main/kotlin/com/supergrecko/questionbot/tools/Arguments.kt
+++ src/main/kotlin/com/supergrecko/questionbot/tools/Arguments.kt
@@ -2,8 +2,16 @@ package com.supergrecko.questionbot.tools
 
 open class Arguments(val args: List<Any?>) {
 
-    inline fun <reified T> asType(index: Int): T? {
-        return args.getOrNull(index) as? T
+    inline fun <reified T> asType(index: Int, onError: () -> Unit = {}): T {
+        val arg = args.getOrNull(index)
+
+        if (arg != null && arg is T) {
+            return arg
+        }
+
+        onError()
+        // This will always fail which is why default() should deal with errors
+        return arg as T
     }
 
     inline fun <reified T> fromList(index: Int, vararg indexes: Int): List<T?> {
