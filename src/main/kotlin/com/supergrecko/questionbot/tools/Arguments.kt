713430283a604a6d7c420759715176187106c7b2
supergrecko <me@supergrecko.com>
2019-09-22 11:50:47 +0200

    refactor(0.0.0): Make Arguments.fromList have an optional null handler

--- src/main/kotlin/com/supergrecko/questionbot/tools/Arguments.kt
+++ src/main/kotlin/com/supergrecko/questionbot/tools/Arguments.kt
@@ -2,6 +2,15 @@ package com.supergrecko.questionbot.tools
 
 open class Arguments(val args: List<Any?>) {
 
+    /**
+     * Get an item from the argument list as type T.
+     *
+     * If item is not T or is null then `onError` will be ran and the final
+     * cast will always fail.
+     *
+     * @param index the index in the list to grab from
+     * @param onError the error handler if item isn't there
+     */
     inline fun <reified T> asType(index: Int, onError: () -> Unit = {}): T {
         val arg = args.getOrNull(index)
 
@@ -14,8 +23,18 @@ open class Arguments(val args: List<Any?>) {
         return arg as T
     }
 
-    inline fun <reified T> fromList(index: Int, vararg indexes: Int): List<T?> {
-        return indexes.map { (args.getOrNull(index) as? List<*>)?.getOrNull(it) as? T }
+    /**
+     * Pick a couple indexes from a List<*> inside the args.
+     *
+     * @param index index in args where List<*> is.
+     * @param indexes the indexes in the List<*> to pick from
+     * @param onError error handler if any of the items are null
+     */
+    inline fun <reified T> fromList(index: Int, vararg indexes: Int, onError: (i: Any?) -> Unit = {}): List<T?> {
+        val result = indexes.map { (args.getOrNull(index) as? List<*>)?.getOrNull(it) as? T }
+        result.filter { it == null }.forEach { item -> onError(item) }
+
+        return result
     }
 
 }
